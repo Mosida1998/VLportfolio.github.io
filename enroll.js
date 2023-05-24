eb87ced037bc1bc6d373e6babc4d4ac42cd50440
Mosida1998 <132605924+Mosida1998@users.noreply.github.com>
2023-05-24 15:41:56 +0800

    Add files via upload
    initial Commit

---
+++ enroll.js
@@ -0,0 +1,11 @@
+function myfunc()
+
+{
+    document.write("HI &nbsp;Good Day<br>"); 
+    document.write("<br>"); 
+    document.write("Thank you For choosing<br>"); 
+    document.write("VL(IT Experts)<br>");
+    document.write("<br>");  
+    document.write("Your appointment date is approved<br>");
+    
+}
